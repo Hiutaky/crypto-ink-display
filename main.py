18605e0347fa23d646c38d569fc3492525f787c9
Alessandro De Cristofaro <hiutaky@gmail.com>
2026-09-24 01:47:25 +0200

    Fix: Use __name__ instead of __main__ for logger

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
 )
-logger = logging.getLogger(__main__)
+logger = logging.getLogger(__name__)
 
 
 class CryptoDisplayApp:
